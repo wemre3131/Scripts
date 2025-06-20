862e7ebeb6a45d2fc432dd444db05215d3c4dcb8
wemre3131 <saoeditoer@gmail.com>
2025-06-20 21:47:59 +0200

    Update bizimscriptler.lua

--- bizimscriptler.lua
+++ bizimscriptler.lua
@@ -1,7 +1,10 @@
+-- Load Kavo UI Library
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
+-- Create main window
 local Window = Library.CreateLib("bizim scriptler", "Sentinel")
 
+-- MAIN TAB
 local MainTab = Window:NewTab("Main")
 local MainSection = MainTab:NewSection("Scripts")
 
@@ -9,82 +12,78 @@ MainSection:NewButton("Infinite Yield", "Admin commands GUI", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
 
-MainSection:NewButton("MM2 Script", "Murder Mystery 2 script", function()
+MainSection:NewButton("MM2 Script", "Murder Mystery 2", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
 end)
 
-MainSection:NewButton("Arsenal Script", "Arsenal", function()
+MainSection:NewButton("Arsenal Script", "Arsenal hacks", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/G6Ubkkuv"))()
 end)
 
-MainSection:NewButton("Fling ALL Script", "Fling all", function()
+MainSection:NewButton("Fling ALL Script", "Fling everyone", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/wemre3131/Scripts/refs/heads/main/Fling"))()
 end)
 
-MainSection:NewButton("Air Hub Script", "Air Hub", function()
+MainSection:NewButton("Air Hub Script", "AirHub universal GUI", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/AirHub.lua"))()
 end)
 
-MainSection:NewButton("Jerk off R6 Script", "Jerk Off", function()
+MainSection:NewButton("Jerk off R6 Script", "NSFW R6 animation", function()
     loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))()
 end)
 
-MainSection:NewButton("Jerk off R15 Script", "Jerk Off", function()
+MainSection:NewButton("Jerk off R15 Script", "NSFW R15 animation", function()
     loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
 end)
 
-MainSection:NewButton("Azure Script", "Azure", function()
+MainSection:NewButton("Azure Script", "Azure GUI", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
 end)
 
-MainSection:NewButton("Blox Fruit Script", "Azure", function()
+MainSection:NewButton("Blox Fruit Script", "Blox Fruits GUI", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
 end)
 
-MainSection:NewButton("Blox Fruit Script", "Blox Fruits", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+MainSection:NewButton("Grow a Garden Script", "Grow a Garden (GAG)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/main/Loader/LoaderV1.lua"))()
 end)
 
-MainSection:NewButton("Grow a Garden Script", "GAG", function()
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
-end)
-
-MainSection:NewButton("Forsaken Script", "Forsaken", function()
+MainSection:NewButton("Forsaken Script", "Forsaken GUI", function()
     loadstring(game:HttpGet("https://rifton.top/loader.lua"))()
 end)
 
-MainSection:NewButton("Bedwars Script", "Kill Aura and More" , function()
+MainSection:NewButton("Bedwars Script", "Kill Aura, fly, etc.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 end)
 
-MainSection:NewButton("Bee Swarm Simulator", "Auto Farm and Auto Find!" , function()
-    loadstring(game:GetObjects("rbxassetid://4384103988")[0X1].Source)("Pepsi Swarm")
+MainSection:NewButton("Bee Swarm Simulator", "Auto Farm, auto find", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PepsiSwarm/BeeSwarmSimulator/main/AutoFarm.lua"))()
 end)
 
-local Player = Window:NewTab("Player")
-local PlayerSection = Player:NewSection("Player")
- 
-PlayerSection:NewSlider("Walkspeed", "Change's your speed", 500, 16, function(s)
+MainSection:NewButton("FE Emote", "Press comma (,) to open menu", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+end)
+
+-- PLAYER TAB
+local PlayerTab = Window:NewTab("Player")
+local PlayerSection = PlayerTab:NewSection("Player Controls")
+
+PlayerSection:NewSlider("Walkspeed", "Changes your walk speed", 500, 16, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
- 
-PlayerSection:NewSlider("Jumppower", "Make's you jump High", 500, 50, function(s)
+
+PlayerSection:NewSlider("Jump Power", "Changes your jump power", 500, 50, function(s)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
- 
-PlayerSection:NewButton("TP Tool", "Click to Teleport" , function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/GuBiX19e"))();
-end)
- 
-PlayerSection:NewButton("Noclip", "Walk threw Walls" , function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/KcZxW1Sp"))();
+
+PlayerSection:NewButton("TP Tool", "Click to teleport", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/GuBiX19e"))()
 end)
 
-PlayerSection:NewButton("R15 To R6 (FE)", "Change's animation" , function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Imagnir/r6_anims_for_r15/main/r6_anims.lua'),true))()
+PlayerSection:NewButton("Noclip", "Walk through walls", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/KcZxW1Sp"))()
 end)
 
-MainSection:NewButton("FE Emote", "All Emotes Keybind Open Is Comma" , function()
-    --keybind to open is comma
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
+PlayerSection:NewButton("R15 to R6 (FE)", "Switch animations to R6 style", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Imagnir/r6_anims_for_r15/main/r6_anims.lua", true))()
 end)
